7badee369dd01d392a293d606ded84482efbeddd
cip <cip@github.com>
2021-04-17 19:25:08 +0300

    commit

---
+++ index.js
@@ -0,0 +1,66 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('btn_send');
+
+  getCookies("https://digionline.ro", "DOSESSV3PRI", (response) => {
+    document.getElementById('DOSESSV3PRI').value = response;
+  });
+
+  getCookies("https://www.digionline.ro", "deviceId", (response) => {
+    document.getElementById('deviceId').value = response;
+  });
+
+  chrome.storage.sync.get('host', function(data) {
+    document.getElementById('host').value = data.host ? data.host : '127.0.0.1' ;
+  });
+
+  checkPageButton.addEventListener('click', function() {
+
+    chrome.tabs.getSelected(null, function(tab) {
+
+      let host =  document.getElementById('host').value;
+      chrome.storage.sync.set({ host: host }); //save setting
+      host = `ws://${host}:9090/jsonrpc`;
+
+      let data = { "jsonrpc": "2.0", 
+                   "method": "Addons.ExecuteAddon", 
+                   "params": { "wait": false, 
+                               "addonid": "plugin.video.digionline", 
+                               "params": { "DOSESSV3PRI": document.getElementById('DOSESSV3PRI').value, 
+                                           "deviceId": document.getElementById('deviceId').value, 
+                                         } 
+                             }, 
+                    "id": 1 };
+
+      const socket = new WebSocket(host);
+      
+      // Listen for possible errors
+      socket.addEventListener('error', function (event) {
+        alert('Error connectimg to ' + host);
+        socket.close();
+      });
+
+      // Connection opened
+      socket.addEventListener('open', function (event) {
+        socket.send(JSON.stringify(data));
+      });
+
+      // Listen for messages
+      socket.addEventListener('message', function (event) {
+        let response  = JSON.parse(event.data);
+        if(response.result == 'OK') {
+          alert('Addon setup successfully!');
+        }
+        socket.close();
+      });
+
+    });
+  }, false);
+}, false);
+
+function getCookies(domain, name, callback) {
+  chrome.cookies.get({"url": domain, "name": name}, (cookie) => {
+    if(callback && cookie) {
+      callback(cookie.value);
+    }
+  });
+}
